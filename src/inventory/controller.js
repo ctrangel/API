a72e34cc7ab7da9eab1c31567c98ed3573bf16d3
ctrangel <122327961+ctrangel@users.noreply.github.com>
2024-02-17 12:02:56 -0500

    ADD: better error handling

--- src/inventory/controller.js
+++ src/inventory/controller.js
@@ -4,32 +4,49 @@ const queries = require("./queries");
 
 const getInventory = (req, res) => {
   pool.query(queries.getInventory, (error, results) => {
-    if (error) throw error;
-    res.status(200).json(results.rows);
+    if (error) {
+      console.error("Database connection error:", error.stack);
+      res.status(500).send("Database connection error");
+    } else {
+      res.status(200).json(results.rows);
+    }
   });
 };
 
 const getInventoryById = (req, res) => {
   const id = parseInt(req.params.id);
   pool.query(queries.getInventoryById, [id], (error, results) => {
-    if (error) throw error;
-    res.status(200).json(results.rows);
+    if (error) {
+      console.error("Database connection error:", error.stack);
+      res.status(500).send("Database connection error");
+    } else {
+      res.status(200).json(results.rows);
+    }
   });
 };
 
 const addJar = (req, res) => {
   const { id, jartype } = req.body;
   pool.query(queries.addJar, [id, jartype], (error, results) => {
-    if (error) throw error;
-    res.status(200).json(results.rows);
+    if (error) {
+      console.error("Database connection error:", error.stack);
+      res.status(500).send("Database connection error");
+    } else {
+      res.status(200).json(results.rows);
+    }
   });
 };
 
 const updateInventory = (req, res) => {
   const { id, jartype } = req.body;
   pool.query(queries.updateInventory, [id, jartype], (error, results) => {
-    if (error) throw error;
-    res.status(200).send("Updated Inventory Successfully");
+     if (error) {
+       console.error("Database connection error:", error.stack);
+       res.status(500).send("Database connection error");
+     } else {
+      res.status(200).send("Updated Inventory Successfully");
+     }
+    
   });
 };
 

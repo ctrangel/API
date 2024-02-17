4a3e794fa14d46e155b6e175c392389685613b2d
ctrangel <122327961+ctrangel@users.noreply.github.com>
2024-02-17 11:43:15 -0500

    FIX: queries

--- db.js
+++ db.js
@@ -1,20 +1,20 @@
 
 const Pool = require("pg").Pool;
 
-// const pool = new Pool({
-//   user: "ctrangel",
-//   password: "n6DdDHS49i4uzMGHgiTAAZxHDXRlMZfq",
-//   host: "dpg-cn1d8hf109ks73cf2qe0-a.ohio-postgres.render.com",
-//   database: "inventory_bcqt",
-//   port: 5432,
-// });
-
 const pool = new Pool({
   user: "ctrangel",
-  password: "Rangel155",
-  host: "localhost",
-  database: "inventory",
+  password: "n6DdDHS49i4uzMGHgiTAAZxHDXRlMZfq",
+  host: "dpg-cn1d8hf109ks73cf2qe0-a.ohio-postgres.render.com",
+  database: "inventory_bcqt",
   port: 5432,
-})
+});
+
+// const pool = new Pool({
+//   user: "ctrangel",
+//   password: "Rangel155",
+//   host: "localhost",
+//   database: "inventory",
+//   port: 5432,
+// })
 
 module.exports = pool;
--- src/inventory/queries.js
+++ src/inventory/queries.js
@@ -2,7 +2,7 @@ const getInventory = "SELECT * FROM jar";
 
 const getInventoryById = "SELECT * FROM wine WHERE id = $1";
 
-const addJar = "INSERT into jar (id,jartype) VALUES($1, $2, $3)";
+const addJar = "INSERT into jar (id,jartype) VALUES($1, $2)";
 
 const updateInventory = "UPDATE jar SET jartype = $2 WHERE id = $1";
 

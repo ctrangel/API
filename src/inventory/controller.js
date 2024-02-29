248823b93fb28c355931837e3e297b098e0fc749
ctrangel <ctrangel@pointpark.edu>
2024-02-29 14:55:25 -0500

    replace bcrypt

--- src/inventory/controller.js
+++ src/inventory/controller.js
@@ -1,7 +1,7 @@
 const { error } = require("console");
 const pool = require("../../db");
 const queries = require("./queries");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Login Controls    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
